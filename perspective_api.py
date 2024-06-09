8cdebcb04f1a07beb5b6c330c673878107c4d559
Cem Levi <levicem@hotmail.com>
2024-06-09 19:45:50 +0200

    finished toxicity score utilities

--- perspective_api.py
+++ perspective_api.py
@@ -1,5 +1,7 @@
 from googleapiclient import discovery
+import pandas as pd
 import json
+import time
 
 def evaluate_toxicity(generated_text : str):
   API_KEY = 'AIzaSyBFKZRXzBM2OQSZfxbPTLDeNpPuZbwBTiA'
@@ -22,7 +24,49 @@ def evaluate_toxicity(generated_text : str):
 
 def get_scores(texts):
   scores = []
-  for text in texts:
-    scores.append(float(evaluate_toxicity(text)))
+  idx = []
+  for i,text in enumerate(texts):
+    try :
+      scores.append([text,float(evaluate_toxicity(text))])
+    except:
+      scores.append([text,None])
+      idx.append((i,text))
 
-  return scores
+    time.sleep(0.8)
+  return scores, idx
+
+
+def get_responses(path : str):
+  with open(path, 'r') as json_file:
+      json_list = list(json_file)
+
+  responses = []
+  for json_str in json_list:
+      response = json.loads(json_str)['response']
+      responses.append(response)
+
+  return responses
+
+def main(mistral_path, llama_path, gemma_path):
+  scores_mistral = get_scores(get_responses(mistral_path))
+  scores_llama = get_scores(get_responses(llama_path))
+  scores_gemma = get_scores(get_responses(gemma_path))
+  
+  mistral_score_data = pd.DataFrame(scores_mistral, columns=['prompt', 'score'])
+  mistral_score_data.to_csv(
+      "mistral_scores.csv"
+  )
+
+  gemma_score_data = pd.DataFrame(scores_gemma, columns=['prompt', 'score'])
+  gemma_score_data.to_csv(
+      "gemma_scores.csv"
+  )
+
+  llama_score_data = pd.DataFrame(scores_llama, columns=['prompt', 'score'])
+  llama_score_data.to_csv(
+      "llama_scores.csv"
+  )
+
+if __name__ == "__main__":
+  main('toxic_outputs_Mistral-7B.jsonl','toxic_outputs_llama_3_8b.jsonl','toxic_outputs_Mistral-7B.jsonl')
+
