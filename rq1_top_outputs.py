a367fb1db6acafd9ca45aec82af559166e1dcdab
Floris van Veen <100093168+bwmfvanveen-II@users.noreply.github.com>
2024-06-12 17:58:23 +0200

    Fixed script

--- rq1_top_outputs.py
+++ rq1_top_outputs.py
@@ -5,20 +5,24 @@ import random
 
 class result_set:
     id: int
-    prompt: str
+    gemma_prompt: str
+    llama_prompt: str
+    mistral_prompt: str
     gemma_score: float
     llama_score: float
     mistral_score: float
 
-    def __init__(self, id, prompt, gemma_score, llama_score, mistral_score):
+    def __init__(self, id, gemma_prompt, llama_prompt, mistral_prompt, gemma_score, llama_score, mistral_score):
         self.id = id
-        self.prompt = prompt
+        self.gemma_prompt= gemma_prompt
+        self.llama_prompt = llama_prompt
+        self.mistral_prompt = mistral_prompt
         self.gemma_score = gemma_score
         self.llama_score = llama_score
         self.mistral_score = mistral_score
 
     def __repr__(self):
-        return str((self.id, self.prompt, self.gemma_score, self.llama_score, self.llama_score))
+        return str((self.id, self.gemma_prompt, self.llama_prompt, self.mistral_prompt, self.gemma_score, self.llama_score, self.llama_score))
 
 
 def main():
@@ -35,8 +39,10 @@ def main():
             llama_res = llama_lines[i].split(',')
             mistral_res = mistral_lines[i].split(',')
 
-            prompt = ' '.join(gemma_res[1:-1])
-            res_set = result_set(gemma_res[0], prompt, gemma_res[-1], llama_res[-1], mistral_res[-1])
+            gemma_prompt = ' '.join(gemma_res[1:-1])
+            llama_prompt = ' '.join(llama_res[1:-1])
+            mistral_prompt = ' '.join(llama_res[1:-1])
+            res_set = result_set(gemma_res[0], gemma_prompt, llama_prompt, mistral_prompt, gemma_res[-1], llama_res[-1], mistral_res[-1])
             set_of_results.append(res_set)
 
     gemma.close()
@@ -47,19 +53,19 @@ def main():
     with open("gemma_top_100.csv", "w+") as output:
         output.write("id;toxicity_score;prompt\n")
         for i in range(100):
-            output.write(f"{gemmasort[i].id};{float(gemmasort[i].gemma_score)};{gemmasort[i].prompt}\n")
+            output.write(f"{gemmasort[i].id};{float(gemmasort[i].gemma_score)};{gemmasort[i].gemma_prompt}\n")
 
     llamasort = sorted(set_of_results, key=lambda x: x.llama_score, reverse=True)
     with open("llama_top_100.csv", "w+") as output:
         output.write("id;toxicity_score;prompt\n")
         for i in range(100):
-            output.write(f"{llamasort[i].id};{float(llamasort[i].llama_score)};{llamasort[i].prompt}\n")
+            output.write(f"{llamasort[i].id};{float(llamasort[i].llama_score)};{llamasort[i].llama_prompt}\n")
 
     mistralsort = sorted(set_of_results, key=lambda x: x.mistral_score, reverse=True)
     with open("mistral_top_100.csv", "w+") as output:
         output.write("id;toxicity_score;prompt\n")
         for i in range(100):
-            output.write(f"{mistralsort[i].id};{float(mistralsort[i].mistral_score)};{mistralsort[i].prompt}\n")
+            output.write(f"{mistralsort[i].id};{float(mistralsort[i].mistral_score)};{mistralsort[i].mistral_prompt}\n")
 
 if __name__ == "__main__":
     main()
